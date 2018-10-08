36794221eb0be68b0284f3c7b2e4255160dd6a2f
Patrick Sodré <psodre@gmail.com>
2018-10-08 12:07:38 -0400

    !fixup

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,4 @@ export CGO_ENABLED=0
 export GOPATH=$SRC_DIR
 
 pushd "$GOPATH/src/github.com/tianon/gosu"
-go build -v -ldflags '-d -s -w' -o "${PREFIX}/bin/gosu"
+go build -v -ldflags '-s -w' -o "${PREFIX}/bin/gosu"
